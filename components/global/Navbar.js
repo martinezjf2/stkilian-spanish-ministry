f39aca0ac8a8e7ea7de9ad790e286bfa58bdddf5
Jeffrey Martinez <martinezjf2@gmail.com>
2025-02-06 16:15:27 -0500

    added the hide scroll feature

--- components/global/Navbar.js
+++ components/global/Navbar.js
@@ -1,14 +1,39 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react"; // Lucide icons for hamburger and close
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  // Handle navbar visibility based on scroll direction
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        // If scrolling down, hide the navbar
+        setShowNavbar(false);
+      } else {
+        // If scrolling up, show the navbar
+        setShowNavbar(true);
+      }
+      setLastScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
 
   return (
-    <nav className="bg-gray-100 text-black px-10 py-2">
-      <div className="max-w-10xl px-6 md:px-12 py-4 flex justify-between items-center">
+    <nav
+      className={`${
+        showNavbar ? "top-0" : "-top-20"
+      } bg-transparent sticky backdrop-blur-md bg-white/10 text-black transition-all duration-500 left-0 w-full z-50 px-10 py-2`}
+    >
+      <div className={`max-w-10xl px-6 md:px-12 py-4 flex justify-between items-center ${showNavbar ? "text-white" : "text-black"}`}>
         {/* Left section: Logo and Links */}
         <div className="flex items-center space-x-8">
           {/* Logo */}
@@ -60,10 +85,12 @@ export default function Navbar() {
               )}
             </div>
 
-            <Link href="/resources/community-events" className="px-2 py-2 hover:underline">
+            <Link
+              href="/resources/community-events"
+              className="px-2 py-2 hover:underline"
+            >
               Events
             </Link>
-
           </div>
         </div>
 
@@ -89,16 +116,10 @@ export default function Navbar() {
             Events
           </Link>
 
-          <Link
-            href="/language"
-            className="block px-5 py-2 hover:underline"
-          >
+          <Link href="/language" className="block px-5 py-2 hover:underline">
             Language
           </Link>
-          <Link
-            href="/login"
-            className="block px-5 py-2 hover:underline"
-          >
+          <Link href="/login" className="block px-5 py-2 hover:underline">
             Log In
           </Link>
         </div>
